c7d501808010505b452cf086a6ba89ebfdc5bcf2
Sanchit <sanchit522@gmail.com>
2020-04-14 19:53:21 +0530

    Updates

---
+++ precache-manifest.fbfdddaec9d30985e72150d572f2a8d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c00a3570f0dd0212ad2778988eeedb8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ba66a12810694dac210b",
+    "url": "/monsters-rolodex/static/css/main.1d087247.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba66a12810694dac210b",
+    "url": "/monsters-rolodex/static/js/main.fc9f39a3.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
